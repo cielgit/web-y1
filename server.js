611a24adff0f9dd106d1224c2f4fa89c605e18ff
cielgit <138621590+cielgit@users.noreply.github.com>
2023-07-10 12:51:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
-//const bcrypt = require('bcrypt')
+
 
 app.use(express.static('.'));
 app.use(express.json())
 
 
-// Définir la route pour l'index.html
+// Définir la route http://127.0.0.1:3001/ pour index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
 
-  // Définir la route pour l'indexTodo.html
+  // Définir la route http://127.0.0.1:3001/todo pour indexTodo.html
 app.get('/todo', (req, res) => {
   res.sendFile(path.join(__dirname, 'indexTodo.html'));
 });
